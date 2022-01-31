b548b9bc42ac6765e3f7ce235d0584909b220594
Henrik Grotle <henrik.grotle@gmail.com>
2022-02-01 00:23:07 +0100

    minor change to PlayerName

--- client/components/PlayerName.js
+++ client/components/PlayerName.js
@@ -1,12 +1,12 @@
 import { useViewModel } from '@resolve-js/react-hooks'
 import React, { useState, useEffect } from 'react'
 
-const PlayerName = ({playerid: aggregateId}) => {
+const PlayerName = ({playerid}) => {
   const [playerName, setPlayerName] = useState("Unknown player")
 
   const { connect, dispose } = useViewModel(
     'PlayerName', // The View Model's name.
-    [aggregateId], // The aggregate ID for which to query data.
+    [playerid], // The aggregate ID for which to query data.
     setPlayerName // A callback to call when new data is recieved.
   )
 
