190684d06b7e53cb5065533bfd87f8062f4e8e11
Henrik Grotle <henrik.grotle@gmail.com>
2022-02-02 21:44:56 +0100

    validate players in match

--- common/aggregates/season.commands.js
+++ common/aggregates/season.commands.js
@@ -1,4 +1,4 @@
-import { isArray } from "lodash";
+import _, { isArray } from "lodash";
 import { SEASON_CREATED, SEASON_MATCH_REGISTERED } from "../event-types";
 export default {
     createSeason: (state, { payload: { leagueid } }) => {
@@ -11,11 +11,14 @@ export default {
     },
     registerMatch: (state, { payload: { matchid, winners, losers }}) => {
       if (!matchid)
-        throw new Error("mathid must be set")
+        throw new Error("matchid must be set")
       if (state.matches[matchid])
         throw new Error("Match has already been registered")
       if (!isArray(winners) || !isArray(losers))
         throw new Error("winners and losers must be arrays")
+      const all = [...winners,...losers]
+      if (_.uniq(all).length != all.length)
+        throw new Error("A user can only appear once in a game")
       return {
         type: SEASON_MATCH_REGISTERED,
         payload: { matchid, winners, losers }
